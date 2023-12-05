42a8a16062c9192a5e28e79a92e44e02d316a1da
Daniel Xerxes <daniel.f.l.r.c@gmail.com>
2023-12-05 11:16:42 -0500

    hmmm2.

--- src/dungeons/gui/Game.java
+++ src/dungeons/gui/Game.java
@@ -7,11 +7,6 @@ import java.awt.Image;
 import java.util.Timer;
 import java.util.TimerTask;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
-
 /**
  *
  * @author educacion
